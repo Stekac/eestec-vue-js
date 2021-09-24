a9ec3a8890857ccec7b35528e724ac1530072394
Stefan Dejanovski <stefan.dejanovski@gmail.com>
2021-09-24 15:43:33 +0200

    voved-vo-vuejs

---
+++ composition-options/src/components/Compostion.vue
@@ -0,0 +1,35 @@
+<script>
+import { ref, computed } from 'vue';
+export default {
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    const number = ref(0);
+
+    const increment = () => {
+      number.value++;
+    }
+
+    const isGreaterThen10 = computed(() => {
+      return number.value > 10;
+    })
+
+    return {
+      number,
+      increment,
+      isGreaterThen10
+    }
+  }
+}
+</script>
+
+<template>
+  <h4>{{ msg }} - {{ number }}</h4>
+  <h4 v-if="isGreaterThen10">Brojot e pogolem od 10</h4>
+  <h4 v-else="!isGreaterThen10">Brojot ne e pogolem od 10</h4>
+  <button v-on:click="increment">Increment</button>
+</template>
+
+<style>
+</style>
---
+++ composition-options/src/components/Options.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  data() {
+    return {
+      value: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.value++;
+    },
+  },
+  computed: {
+    isGreatedThan10() {
+      return this.value > 10;
+    }
+  }
+}
+</script>
+
+<template>
+  <h4>{{ msg }} - {{ value }}</h4>
+  <h4 v-if="isGreatedThan10">Brojot e pogolem od 10</h4>
+  <h4 v-else="!isGreatedThan10">Brojot ne e pogolem od 10</h4>
+  <button v-on:click="increment">Increment</button>
+</template>
+
+<style>
+</style>
